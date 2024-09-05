280282ad6de3c684357a21f09918b6a21728b91d
alaba98 <83726565+alaba98@users.noreply.github.com>
2024-09-05 20:24:44 +0100

    minor bug tweak

--- src/auth.jsx
+++ src/auth.jsx
@@ -1,5 +1,5 @@
 const clientId = '803d5e70b3d14cdb8b06f9106356c920'; 
-const redirectUri = 'http://localhost:5173/callback'; 
+const redirectUri = 'http://localhost:4173/callback'; 
 const scopes = 'streaming user-read-email user-read-private'; 
 
 export const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
